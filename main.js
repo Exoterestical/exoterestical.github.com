7d072cf1428960f716f2378eb03e7b9f01260f14
Exoterestical <49740027+Exoterestical@users.noreply.github.com>
2021-04-27 13:27:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1208,7 +1208,7 @@ function loadData() {
     "hl-d": /((weapon|assault|close range|long range) damage|firepower)/gi,
     "hl-e": /(weakness|vulnerab(le|ility)|marked|burn(ing)?|ash(ed)?|bleed|toxic|freeze|frozen)/gi,
     "hl-h": /((maximum )?Leben( regen)|Leben?)/gi,
-    "hl-l": /(^\[hm\]|(weapon|skill) leech|heil(?!t)(s|en|ung)?)/gi,
+    "hl-l": /(^\[hm\]|(weapon|skill) leech|?heil(?!t)(s|en|ung)?)/gi,
     "hl-n": /((concentration|magma golem|anomaly in veins|br\/8 impact amplifier) node)/gi,
     "hl-p": /((armor|(armor and )?resistance) piercing)/gi,
     "hl-r": /((armor|resistance)(?!( and resistance)? piercing)|damage mitigation)/gi,
